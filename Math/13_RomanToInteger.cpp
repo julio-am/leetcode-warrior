3ee7ff1fc7c80222a5da4994ab93de8855144274
Julio A. Medina <65096300+julio-am@users.noreply.github.com>
2022-11-08 12:51:42 -0800

    do math

---
+++ Math/13_RomanToInteger.cpp
@@ -0,0 +1,21 @@
+class Solution {
+public:
+    int romanToInt(string s) {
+        if (s.length() == 0) return 0;
+        
+        map<char, int> values = {
+            {'I', 1}, {'V', 5}, {'X', 10}, {'L', 50},
+            {'C', 100}, {'D', 500}, {'M', 1000} };
+        
+        int result = 0;
+        for (int i = 0; i < s.length()-1; ++i) {
+            if (values[s[i]] < values[s[i+1]])
+                result -= values[s[i]];
+            else
+                result += values[s[i]];   
+        }
+        
+        result += values[s.back()];
+        return result;
+    }
+};
