66d7a26fa830631b57102443908d7a137c345837
Julio A. Medina <65096300+julio-am@users.noreply.github.com>
2024-09-29 22:17:23 -0700

    Update LowestCommonAncestor.cpp

--- Graphs/LowestCommonAncestor.cpp
+++ Graphs/LowestCommonAncestor.cpp
@@ -11,25 +11,23 @@ class Solution {
 private:
     TreeNode* result;
 
-
     // recurse through the tree and set the result to be the first node sych that 
     // either the left subtree, the right subtree, or itself contain both p and q.
     bool recurseTree(TreeNode* root, TreeNode* p, TreeNode* q) {
         if (!root) return false;
-        
-        int curr = (root == p || root == q) ? 1 : 0;
-        int left = recurseTree(root->left, p, q) ? 1 : 0;
-        int right = recurseTree(root->right, p, q) ? 1 : 0;
 
-        if (left + curr + right >= 2) result = root;
+        bool curr = root == p || root == q;
+        bool left = recurseTree(root->left, p, q);
+        bool right = recurseTree(root->right, p, q);
 
-        return left + curr + right > 0;
+        if (left + right + curr >= 2)  result = root;
+
+        return left || right || curr;
     }
 
 public:
     TreeNode* lowestCommonAncestor(TreeNode* root, TreeNode* p, TreeNode* q) {
         recurseTree(root, p, q);
-
         return result;
     }
 };
