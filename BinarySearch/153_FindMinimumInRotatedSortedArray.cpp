df819d158fe9e8b67dc70fd98697f5661579033e
Julio A. Medina <65096300+julio-am@users.noreply.github.com>
2026-06-25 15:25:18 -0700

    Create 153_FindMinimumInRotatedSortedArray.cpp

---
+++ BinarySearch/153_FindMinimumInRotatedSortedArray.cpp
@@ -0,0 +1,29 @@
+class Solution {
+public:
+    int findMin(vector<int> &nums) {
+        int l = 0, r = nums.size()-1;
+        int res = INT_MAX;
+
+        while (l <= r) {
+            int m = l + (r-l) / 2;
+            
+            // if the leftmost value is smaller than the rightmost value,
+            // our current search window is propely sorted. so we return early.
+            if (nums[l] < nums[r]) return min(res, nums[l]);
+ 
+            res = min(res, nums[m]);
+
+            // If the array is not sorted, but
+            // the middle value is bigger than the left value, the pivot must be 
+            // on the right side, since all values on the right of the pivot must be 
+            // smaller than all values on the left of the pivot. 
+            if (nums[m] >= nums[l]) l = m+1; 
+
+            // If the middle value is smaller than the left value, the pivot mu16string
+            // be somewhere to the left of the middle value.
+            else r = m-1;
+        }
+
+        return res;
+    }
+};
