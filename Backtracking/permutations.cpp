87c142bf35217ef2cc5ee5745bc800a3e8d7e1e5
Julio A. Medina <65096300+julio-am@users.noreply.github.com>
2026-07-01 19:42:15 -0700

    Update permutations.cpp

--- Backtracking/permutations.cpp
+++ Backtracking/permutations.cpp
@@ -15,79 +15,25 @@
 //https://www.geeksforgeeks.org/write-a-c-program-to-print-all-permutations-of-a-given-string/
 
 //================================
-
-template<class T>
-class Permuter {
-
-  private:
-    // Array to store the answer 
-    std::vector<std::vector<T> > result;
-
-    // Computes all permutations of a given array
-    void permute(std::vector<T> arr, int l, int r) {
-      // Base case
-      if (l == r) {
-          result.push_back(arr);
-      }
-
-      // Make permutation
-      for int (i=l; i<=r; ++i) {
-          // Swap start and the current position
-          swap(arr[l], arr[i]);
-
-          // recurse on the next segment
-          permute(arr, l+1, r)
-
-          swap(arr[l], arr[i]);
-      }
-    }
-
-
-  public:
-
-    std::vector<std::vector<T> > getPermutations(std::vector<T> arr) {
-      permute(arr, 0, arr.size());    
-      return result;    
-    }
-};
-
-
---------------------------------------
-
-
-void permute(vector<int> arr, int start, int end) {
-  if (l == r) {
-    results.push_back(arr);
-  }
-
-  else (
-    for (int i = start; i <= end; ++i) {
-      arr.swap(i, start);
-      permute(arr, start+1, end);
-      arr.swap(i, start);
-    }
-  )
-}
-
-------------------------------------
-
 class Solution {
-public:
-    vector<vector<int>> permute(vector<int>& nums) {
-        if (nums.size() == 0) return {{}};
 
-        vector<int> remaining_nums(nums.begin() + 1, nums.end());
-        vector<vector<int>> perms = permute(remaining_nums);
-        vector<vector<int>> result;
+private:
 
-        for (auto perm : perms) {
-            for (int i = 0; i <= perm.size(); ++i) {
-                perm.insert(perm.begin()+i, nums[0]);
-                result.push_back(perm);
-                perm.erase(perm.begin()+i);
-            }
-        }
+vector<vector<int>> result;
 
-        return result;
+void backtrack(vector<int>& nums, int l) {
+    int r = nums.size()-1;
+
+    // if we have reached the end of nums, append our current permutation and return.
+    if (l == r) {
+        result.push_back(nums);
+        return;
     }
-};
+
+    // swap every rightward number into L, then do backtracking call on l+1.
+    for (int i = l; i <= r; ++i) {
+        swap(nums[l], nums[i]);
+        backtrack(nums, l+1);
+        swap(nums[l], nums[i]);
+    }
+}
