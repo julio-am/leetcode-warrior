227da66333cd4f8774dce2d0040236f4de6adbec
Julio A. Medina <65096300+julio-am@users.noreply.github.com>
2026-07-30 16:44:58 -0700

    Update MaximumSubarray.cpp

--- Greedy/MaximumSubarray.cpp
+++ Greedy/MaximumSubarray.cpp
@@ -1,31 +1,18 @@
 class Solution {
-public:
     
-    int maxSubArray(vector<int>& nums) { 
-        int curr = nums[0];
-        int result = curr;
+public:
+int maxSubArray(vector<int>& nums) {
+    int curr = 0;
+    int result = INT_MIN;
 
-        for (int i = 1; i<nums.size(); ++i) {
-            curr = max(curr+nums[i], nums[i); //choose to either use prefix or not
-            result = max(result, curr); //choose to either use previous result or current
-        }
+    for (int n : nums) {
+        // Either extend the previous subarray or start a new one at n.
+        curr = max(curr + n, n);
 
-        return result;
+        // Record the best subarray seen so far.
+        result = max(result, curr);
     }
 
-
-  //alternately
-  int maxSubArray(vector<int>& nums) { 
-      int curr = nums[0];
-      int result = curr;
-
-      for (int i = 1; i<nums.size(); ++i) {
-          if (curr < 0) curr = 0;
-          curr += nums[i];
-          result = max(result, curr);
-      }
-  
-      return result;
-  }
-
+    return result;
+}
 };
